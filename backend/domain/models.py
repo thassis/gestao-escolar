a19a79d31813b5aed10c0be62ec393432781beea
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2023-05-14 20:59:15 -0300

    #4 Inicia implementação da camada de domínio
    Adiciona classes Aluno, Presenca e Professor
    
    Adiciona serviço para Aluno

---
+++ backend/domain/models.py
@@ -0,0 +1,35 @@
+"""Domain models for the school management system."""
+
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass
+class Aluno:
+    """Aluno model class for domain layer."""
+    id: int
+    name: str
+    born_date: date
+    address: str
+    tutor_name: str
+    tutor_phone: str
+    class_shift: str
+
+
+@dataclass
+class Presenca:
+    """Presenca model class for domain layer."""
+    id: int
+    aluno_id: int
+    status: str
+    document: str
+    reason_missing_class: str
+
+
+@dataclass
+class Professor:
+    """Professor model class for domain layer."""
+    id: int
+    name: str
+    login: str
+    password: str
