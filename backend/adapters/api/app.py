652428dfa34d08dd260c85c75938dca2b6664820
misaelrezende <38136358+misaelrezende@users.noreply.github.com>
2023-05-24 21:29:06 -0300

    Fix routes path Passa porta como parametro na chamada do app flask
    Co-authored-by: fcmf <fcmf@users.noreply.github.com>

--- backend/adapters/api/app.py
+++ backend/adapters/api/app.py
@@ -1,9 +1,12 @@
+import sys
 from flask import Flask
 from routes import app as routes_app
+
+
 app = Flask(__name__)
 
 
 app.register_blueprint(routes_app)
 
 if __name__== '__main__':
-    app.run()
+    app.run(port=str(sys.argv[1]))
--- backend/adapters/api/routes.py
+++ backend/adapters/api/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 import sys
-sys.path.append('../../../')
+sys.path.append('./')
+
 from backend.adapters.controllers.controller import AlunoController, ProfessorController
 
 app = Blueprint('routes', __name__)
