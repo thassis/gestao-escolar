afa12cbfbed541c48d8e71d5b4aa24d55fb02443
danielffds1 <danielffds8@gmail.com>
2023-05-30 21:15:30 -0300

    Configuração dos eventos antigos

---
+++ frontend/src/data-front/oldEvent.js
@@ -0,0 +1,51 @@
+import Tecnologia from "../images/tecnologia.jpg";
+import Saude from "../images/saude.jpg";
+import Artes from "../images/artes.jpeg";
+import BemEstar from "../images/bem-estar.jpeg";
+import Esporte from "../images/esporte.jpg";
+import MeioAmbiente from "../images/meio-ambiente2.jpg";
+
+export const oldEvent = [
+  {
+    id: 1,
+    name: "Evento 1",
+    date: new Date("1/4/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: Tecnologia,
+  },
+  {
+    id: 2,
+    name: "Evento 2",
+    date: new Date("8/4/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: Saude,
+  },
+  {
+    id: 3,
+    name: "Evento 3",
+    date: new Date("9/4/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: Artes,
+  },
+  {
+    id: 4,
+    name: "Evento 4",
+    date: new Date("10/4/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: BemEstar,
+  },
+  {
+    id: 5,
+    name: "Evento 5",
+    date: new Date("1/5/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: Esporte,
+  },
+  {
+    id: 6,
+    name: "Evento 6",
+    date: new Date("2/5/2023"),
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue.',
+    image: MeioAmbiente,
+  },
+];
