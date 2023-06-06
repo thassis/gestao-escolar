8c7eb705602898fe5f90991efb8c0094fbe1fe54
danielffds1 <danielffds8@gmail.com>
2023-06-06 18:17:36 -0300

    Retificando os testes unitários do frontend

--- frontend/src/shared-components/header/Header.test.tsx
+++ frontend/src/shared-components/header/Header.test.tsx
@@ -1,40 +1,44 @@
 import '@testing-library/jest-dom/extend-expect';
 import React from "react";
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from 'react-router-dom';
 
 import Header from "./Header";
 
-
-jest.mock("react-router-dom");
-
 describe("Header component", () => {
-
   it("renders title correctly", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
     const titleElement = screen.getByText("Sistema de");
-    //assert
     expect(titleElement).toBeInTheDocument();
   });
 
   it("renders subtitle correctly", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
     const subtitleElement = screen.getByText("gestão escolar");
-    //assert
     expect(subtitleElement).toBeInTheDocument();
   });
-  
 
   it("renders navigation buttons correctly", () => {
-    render(<Header />);
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
     const homeButton = screen.getByText("Home");
-    const presencaButton = screen.getByText("Presença");
-    const relatoriosButton = screen.getByText("Relatórios");
+    const presencaButton = screen.getByText("Presenca");
+    const relatoriosButton = screen.getByText("Relatorio");
     const eventosButton = screen.getByText("Eventos");
-    //assert
     expect(homeButton).toBeInTheDocument();
     expect(presencaButton).toBeInTheDocument();
     expect(relatoriosButton).toBeInTheDocument();
     expect(eventosButton).toBeInTheDocument();
   });
-
 });
--- frontend/src/shared-components/header/Header.tsx
+++ frontend/src/shared-components/header/Header.tsx
@@ -57,7 +57,7 @@ const Header = () => {
       <NavBox>
         <ListItemLink 
           to="/home" 
-          label={"home"} 
+          label={"Home"} 
           onClick={toggleDrawerOpen}
         />
         <ListItemLink 
